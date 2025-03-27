b261357af899e45644fdd5ca4bf4537ed2790b87
Sarvajith Adyanthaya <109269103+Chillthrower@users.noreply.github.com>
2025-03-27 07:28:27 -0700

    Update Hamming.cpp

--- Hamming.cpp
+++ Hamming.cpp
@@ -1,20 +1,29 @@
 #include <stdio.h>
 
-void encode(int d[4], int h[7]) 
+int main() 
 {
+    int d[4], h[7];
+
+    printf("Data: "); 
+    for (int i = 0; i < 4; i++) scanf("%1d", &d[i]);
+
     h[6] = d[0]; 
     h[5] = d[1]; 
     h[4] = d[2]; 
     h[2] = d[3];
 
     h[0] = h[2] ^ h[4] ^ h[6]; 
-    h[1] = h[2] ^ h[5] ^ h[6];  //0246 1256 3456
+    h[1] = h[2] ^ h[5] ^ h[6];  
     h[3] = h[4] ^ h[5] ^ h[6];
-}
 
-void correct(int h[7]) 
-{
-    int e = (h[0] ^ h[2] ^ h[4] ^ h[6]) | ((h[1] ^ h[2] ^ h[5] ^ h[6]) << 1) | ((h[3] ^ h[4] ^ h[5] ^ h[6]) << 2); //0246 1256 3456
+    printf("Hamming: "); 
+    for (int i = 6; i >= 0; i--) printf("%d", h[i]); 
+    printf("\n");
+
+    printf("Received: "); 
+    for (int i = 6; i >= 0; i--) scanf("%1d", &h[i]);
+
+    int e = (h[0] ^ h[2] ^ h[4] ^ h[6]) | ((h[1] ^ h[2] ^ h[5] ^ h[6]) << 1) | ((h[3] ^ h[4] ^ h[5] ^ h[6]) << 2); 
 
     if (e)
     {
@@ -24,31 +33,11 @@ void correct(int h[7])
 
     printf("Code: "); 
     for (int i = 6; i >= 0; i--) printf("%d", h[i]); 
-    
     printf("\n");
-}
-
-int main() 
-{
-    int d[4], h[7];
-
-    printf("Data: "); 
-    for (int i = 0; i < 4; i++) scanf("%1d", &d[i]);
-
-    encode(d, h);
-
-    printf("Hamming: "); 
-    for (int i = 6; i >= 0; i--) printf("%d", h[i]); 
-    
-    printf("\n");
-
-    printf("Received: "); 
-    for (int i = 6; i >= 0; i--) scanf("%1d", &h[i]);
-
-    correct(h);
 
     return 0;
 }
+
 
 
 //Example

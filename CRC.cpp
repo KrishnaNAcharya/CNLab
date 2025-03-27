7eeb204c5e90d7a7644bf79eca88bf034b2a0db6
Sarvajith Adyanthaya <109269103+Chillthrower@users.noreply.github.com>
2025-03-27 07:39:49 -0700

    Update CRC.cpp

--- CRC.cpp
+++ CRC.cpp
@@ -1,32 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
-void crc(char *msg, char *rem, char *gen) 
-{
-    int glen = strlen(gen);
-    int mlen = strlen(msg) - glen + 1;
-    
-    strncpy(rem, msg, glen);
-    for (int i = 0; i < mlen; i++) 
-    {
-        if (rem[0] == '1')
-        {
-            for (int j = 1; j < glen; j++)
-            {
-                rem[j] = (rem[j] == gen[j]) ? '0' : '1';
-            }
-        }
-        
-        memmove(rem, rem + 1, glen);
-        rem[glen - 1] = msg[i + glen];
-    }
-    rem[glen] = '\0';
-}
-
 int main() 
 {
     char data[128], gen[32], code[160], rem[32], recv[160];
     int pos, test;
+    
     printf("No of bits: ");
     scanf("%d", &test);
     printf("Data: ");
@@ -34,27 +13,43 @@ int main()
     printf("Gen: ");
     scanf("%s", gen);
     
-    // Append zeros
-    sprintf(code, "%s%0*d", data, strlen(gen)-1, 0);
+    // Append zeros to data
+    sprintf(code, "%s%0*d", data, strlen(gen) - 1, 0);
     printf("Modified data: %s\n", code);
+
+    // CRC Calculatio
+    int glen = strlen(gen);
+    int mlen = strlen(code) - glen + 1;
     
-    // Calculate CRC
-    crc(code, rem, gen);
+    strncpy(rem, code, glen);
+    for (int i = 0; i < mlen; i++) 
+    {
+        if (rem[0] == '1')
+        {
+            for (int j = 1; j < glen; j++)
+                rem[j] = (rem[j] == gen[j]) ? '0' : '1';
+        }
+        
+        memmove(rem, rem + 1, glen);
+        rem[glen - 1] = code[i + glen];
+    }
+    rem[glen] = '\0';
+    
     printf("Checksum: %s\n", rem);
-    
-    // Create final codeword
+
     strcpy(code + strlen(data), rem);
     printf("Final codeword: %s\n", code);
-    
-    // Error insertion
+
     printf("Error position: ");
     scanf("%d", &pos);
+    
     strcpy(recv, code);
     recv[pos] = (recv[pos] == '0') ? '1' : '0';
     printf("Data with error: %s\n", recv);
-    
+
     return 0;
 }
+
 
 //Example
 // No of bits: 8

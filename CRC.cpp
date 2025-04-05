a04a2e3850ce99cdd2f4330a2eebba45d78aee37
Krishna N Acharya <142175482+KrishnaNAcharya@users.noreply.github.com>
2025-04-05 20:52:49 +0530

    Update CRC.cpp

--- CRC.cpp
+++ CRC.cpp
@@ -1,47 +1,44 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() 
+int main()
 {
     char data[128], gen[32], code[160], rem[32], recv[160];
-    int pos, test;
-    
-    printf("Enter No of bits, Data, Gen: ");
-    scanf("%d %s %s", &test, data, gen);    
-    
-    sprintf(code, "%s%0*d", data, strlen(gen) - 1, 0);
-    printf("Modified data: %s\n", code);
+    int pos, i, j;
 
-    int glen = strlen(gen);
-    int mlen = strlen(code) - glen + 1;
-    
+    printf("Enter Data and Generator: ");
+    scanf("%s %s", data, gen);
+
+    int dlen = strlen(data), glen = strlen(gen);
+    sprintf(code, "%s%0*d", data, glen - 1, 0);
+    printf("Modified Data: %s\n", code);
+
     strncpy(rem, code, glen);
-    for (int i = 0; i < mlen; i++) 
+    for (i = 0; i <= strlen(code) - glen; i++)
     {
         if (rem[0] == '1')
-        {
-            for (int j = 1; j < glen; j++)
+            for (j = 1; j < glen; j++)
                 rem[j] = (rem[j] == gen[j]) ? '0' : '1';
-        }
-        
-        memmove(rem, rem + 1, glen);
+
+        memmove(rem, rem + 1, glen - 1);
         rem[glen - 1] = code[i + glen];
     }
-    rem[glen] = '\0';
-    strcpy(code + strlen(data), rem);
-    strcpy(recv, code);
-    
-    printf("Checksum: %s\n", rem);
-    printf("Final codeword: %s\n", code);
+    rem[glen - 1] = '\0';
 
-    printf("Error position: ");
+    strcpy(code + dlen, rem);
+    strcpy(recv, code);
+
+    printf("Checksum: %s\n", rem);
+    printf("Codeword: %s\n", code);
+
+    printf("Enter error position (0-based): ");
     scanf("%d", &pos);
-    
-    recv[pos] = (recv[pos] == '0') ? '1' : '0';
+    recv[pos] ^= 1; // Flip the bit using XOR
     printf("Data with error: %s\n", recv);
 
     return 0;
 }
+
 
 
 //Example

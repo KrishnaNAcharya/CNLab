4dc468cc7fb80f26edef32845703ecc76c416c2c
Krishna N Acharya <142175482+KrishnaNAcharya@users.noreply.github.com>
2025-04-09 15:19:38 +0530

    Update client.cpp

--- client-server/client.cpp
+++ client-server/client.cpp
@@ -5,21 +5,22 @@
 
 int main() 
 {
-    printf("Enter File name: \n");
+    printf("Connected to server.\n\n");
 
     int s = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in a = {AF_INET, htons(65535)};
+    struct sockaddr_in a = {AF_INET, htons(60000)};
     connect(s, (sockaddr*)&a, sizeof(a));
 
     char f[9], b[99];
 
-    while (printf(">"), scanf("%s", f), strcmp(f, "q")) 
+    while (printf("\n Enter filename (or type 'exit' to quit):\n"), scanf("%s", f), strcmp(f, "exit")) 
     {
         send(s, f, strlen(f) + 1, 0);
+        printf("\nServer Response:\n");
         recv(s, b, sizeof(b), 0);
         printf("%s", b);
     }
 
-    send(s, "q", 2, 0);
+    send(s, "exit", 2, 0);
     close(s);
 }

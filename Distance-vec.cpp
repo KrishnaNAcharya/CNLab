c38f110d687dad4cd9d83ea9a71ff5585c437a12
Krishna N Acharya <142175482+KrishnaNAcharya@users.noreply.github.com>
2025-04-09 13:18:08 +0530

    Update Distance-vec.cpp

--- Distance-vec.cpp
+++ Distance-vec.cpp
@@ -1,21 +1,21 @@
-//5. Write a program for a distance vector algorithm to find a suitable path for transmission.
+//2. Write a program for a distance vector algorithm to find a suitable path for transmission.
 
 #include <stdio.h>
-#define INF 999
 
-struct node 
-{ 
-    int dist[20]; 
-    int from[20]; 
+struct node
+{
+    int dist[20];
+    int from[20];
 } rt[10];
 
-int main() {
-    int n, d[20][20]; 
-    
-    printf("Nodes: "); 
+int main()
+{
+    int n, d[20][20];
+
+    printf("Enter number of nodes: ");
     scanf("%d", &n);
 
-    printf("Cost matrix:\n");
+    printf("Enter the distance matrix:\n");
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
         {
@@ -24,9 +24,10 @@ int main() {
             rt[i].from[j] = j;
         }
 
-    int change;
-    do {
-        change = 0;
+    int flag;
+    do
+    {
+        flag = 0;
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
                 for (int k = 0; k < n; k++)
@@ -34,45 +35,63 @@ int main() {
                     {
                         rt[i].dist[j] = d[i][k] + rt[k].dist[j];
                         rt[i].from[j] = k;
-                        change = 1;
+                        flag = 1;
                     }
-    } while (change);
+    } while (flag);
 
-    for (int i = 0; i < n; i++) {
-        printf("\nRouter %d:\nDest\tVia\tDist\n", i + 1);
+    for (int i = 0; i < n; i++)
+    {
+        printf("\nRouter information for router:%d:\nDestination\tNextHop\t\tDistance\n", i + 1);
         for (int j = 0; j < n; j++)
-            printf("%d\t%d\t%d\n", j + 1, rt[i].from[j] + 1, rt[i].dist[j]);
+            printf("%d\t\t%d\t\t%d\n", j + 1, rt[i].from[j] + 1, rt[i].dist[j]);
     }
 }
 
 
 //Example
-// Nodes: 3
-// Cost matrix:
+// Enter number of nodes: 4
+// Enter the distance matrix:
 // 0
-// 1
-// 999
-// 1
+// 3
+// 5
+// 99
+// 3
 // 0
+// 99
 // 1
-// 999
+// 5
+// 4
+// 0
+// 2
+// 99
 // 1
+// 2
 // 0
 
-// Router 1:
-// Dest    Via     Dist
-// 1       1       0
-// 2       2       1
-// 3       2       2
+// Router information for router:1:
+// Destination     NextHop         Distance
+// 1               1               0
+// 2               2               3
+// 3               3               5
+// 4               2               4
 
-// Router 2:
-// Dest    Via     Dist
-// 1       1       1
-// 2       2       0
-// 3       3       1
+// Router information for router:2:
+// Destination     NextHop         Distance
+// 1               1               3
+// 2               2               0
+// 3               4               3
+// 4               4               1
 
-// Router 3:
-// Dest    Via     Dist
-// 1       2       2
-// 2       2       1
-// 3       3       0
+// Router information for router:3:
+// Destination     NextHop         Distance
+// 1               1               5
+// 2               4               3
+// 3               3               0
+// 4               4               2
+
+// Router information for router:4:
+// Destination     NextHop         Distance
+// 1               2               4
+// 2               2               1
+// 3               3               2
+// 4               4               0

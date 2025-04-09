be33403abde2c70edc51cf328f33b78edc72ddc1
Krishna N Acharya <142175482+KrishnaNAcharya@users.noreply.github.com>
2025-04-09 09:49:59 +0530

    Update Hamming.cpp

--- Hamming.cpp
+++ Hamming.cpp
@@ -2,31 +2,32 @@
 
 #include <stdio.h>
 
-int main() 
+int main()
 {
     int d[4], h[7];
 
-    printf("Data: "); 
+    printf("Data: ");
     for (int i = 0; i < 4; i++) scanf("%1d", &d[i]);
-
-    h[6] = d[0]; 
-    h[5] = d[1]; 
-    h[4] = d[2]; 
+        
+    h[6] = d[0];
+    h[5] = d[1];
+    h[4] = d[2];
     h[2] = d[3];
 
     h[0] = h[2] ^ h[4] ^ h[6]; // 0246
-    h[1] = h[2] ^ h[5] ^ h[6]; // 1256 
+    h[1] = h[2] ^ h[5] ^ h[6]; // 1256
     h[3] = h[4] ^ h[5] ^ h[6]; // 3456
 
-    printf("Hamming: "); 
-    for (int i = 6; i >= 0; i--) printf("%d", h[i]); 
+    printf("Hamming: ");
+    for (int i = 6; i >= 0; i--)  printf("%d", h[i]);
     printf("\n");
 
-    printf("Received: "); 
+    printf("Received: ");
     for (int i = 6; i >= 0; i--) scanf("%1d", &h[i]);
+        
 
     // 0246 1256 3456
-    int e = (h[0] ^ h[2] ^ h[4] ^ h[6]) | ((h[1] ^ h[2] ^ h[5] ^ h[6]) << 1) | ((h[3] ^ h[4] ^ h[5] ^ h[6]) << 2); 
+    int e = (h[0] ^ h[2] ^ h[4] ^ h[6]) | ((h[1] ^ h[2] ^ h[5] ^ h[6]) << 1) | ((h[3] ^ h[4] ^ h[5] ^ h[6]) << 2);
 
     if (e)
     {
@@ -34,10 +35,8 @@ int main()
         h[e - 1] ^= 1;
     }
 
-    printf("Code: "); 
-    for (int i = 6; i >= 0; i--) printf("%d", h[i]); 
-    printf("\n");
-
+    printf("Code: ");
+    for (int i = 6; i >= 0; i--)  printf("%d", h[i]);   
     return 0;
 }
 
@@ -58,3 +57,19 @@ int main()
 // 1
 // Error at 6
 // Code: 1111111
+
+
+// Data: 1
+// 0
+// 1
+// 0
+// Hamming: 1010101
+// Received: 1
+// 1
+// 1
+// 0
+// 0
+// 1
+// 0
+// Error at 6
+// Code: 1010010
